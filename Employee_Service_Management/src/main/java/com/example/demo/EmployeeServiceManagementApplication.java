6141872283dcef018785daad55f292a62944ece7
Rajput <maheramol00@gmail.com>
2025-07-31 16:07:20 +0530

    hello

--- Employee_Service_Management/src/main/java/com/example/demo/EmployeeServiceManagementApplication.java
+++ Employee_Service_Management/src/main/java/com/example/demo/EmployeeServiceManagementApplication.java
@@ -9,6 +9,7 @@ public class EmployeeServiceManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceManagementApplication.class, args);
 		System.out.println("Hello World");
+		System.out.println("Done");
 	}
 
 }
